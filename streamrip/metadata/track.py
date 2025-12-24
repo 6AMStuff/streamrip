086bea111f6f95c402488bcf5dc483716390bd7b
Thomas de Lachaux <21559770+ThomasLachaux@users.noreply.github.com>
2025-12-24 15:46:06 +0100

    feat(track): add id field to formatter
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@proton.me>

--- streamrip/metadata/track.py
+++ streamrip/metadata/track.py
@@ -234,6 +234,7 @@ class TrackMetadata:
         # and "explicit", "albumcomposer"
         none_text = "Unknown"
         info = {
+            "id": self.info.id,
             "title": self.title,
             "tracknumber": self.tracknumber,
             "artist": self.artist,
